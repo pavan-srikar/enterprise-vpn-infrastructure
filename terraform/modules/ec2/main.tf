1cdcd05ff6c60a10aa5a47c7a33c0482e108cbd3
nameless <user@mail.com>
2026-06-10 14:32:45 +0530

    changed EC2 to ubuntu

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,5 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "vpn_server" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
 
@@ -8,6 +24,10 @@ resource "aws_instance" "vpn_server" {
   vpc_security_group_ids = [
     var.security_group_id
   ]
+
+  metadata_options {
+    http_tokens = "required"
+  }
 
   tags = {
     Name = "Enterprise-VPN-Gateway"
